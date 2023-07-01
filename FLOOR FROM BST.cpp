5bcae45d6822aa0f4f82471d0b560cb0132544ac
Adrish Bhattacharya <88700100+iadrish@users.noreply.github.com>
2023-07-02 00:40:29 +0530

    Add files via upload

---
+++ FLOOR FROM BST.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int floorInBST(TreeNode<int> * root, int X)
+{
+    // Write your code here.
+    int floor=-1;
+    while(root)
+    {
+        if(root->val==X)
+        {
+            floor=root->val;
+            return floor;
+        }
+        if(root->val>X)
+        {
+            root=root->left;
+        }
+        else
+        {
+            floor=root->val;
+            root=root->right;
+        }
+    }
+    return floor;
+}
